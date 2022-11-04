515b08039dca5d9d2ca2db9de4c59e895b258b2d
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-04 10:41:20 +0900

    feat: MetaTag 수정

--- src/components/Detail/MetaTag.jsx
+++ src/components/Detail/MetaTag.jsx
@@ -11,13 +11,13 @@ const MetaTag = ({ carData }) => {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description || ''} />
+      <meta name="keywords" content={keywords || ''} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:site_name" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={carData.attribute.imageUrl} />
+      <meta property="og:title" content={title || ''} />
+      <meta property="og:site_name" content={title || ''} />
+      <meta property="og:description" content={description || ''} />
+      <meta property="og:image" content={carData.attribute.imageUrl || ''} />
       <meta property="og:url" content={url} />
       <link rel="canonical" href={url} />
     </Helmet>
