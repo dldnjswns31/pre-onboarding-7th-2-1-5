552181703941f5b3b8f45ee89d0b9852fa87612c
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-04 10:07:44 +0900

    feature: prettier 오류 수정

--- src/components/Detail/MetaTag.jsx
+++ src/components/Detail/MetaTag.jsx
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet-async';
 import { useSelectCar } from '../../context/CarContext';
 import comma from '../../utils/comma';
 
-const MetaTag = ({carData}) => {
+const MetaTag = ({ carData }) => {
   const title = carData.attribute.brand + carData.attribute.name;
   const url = window.location.href;
   const description = '월' + comma(carData.amount) + '원부터';
@@ -13,21 +13,17 @@ const MetaTag = ({carData}) => {
   return (
       <Helmet>
         <title>{title}</title>
-
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
-
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:site_name" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={carData.attribute.imageUrl} />
         <meta property="og:url" content={url} />
-
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={carData.attribute.imageUrl} />
-
         <link rel="canonical" href={url} />
       </Helmet>
   );
