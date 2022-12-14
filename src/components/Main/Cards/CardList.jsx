780be49232e3a4af28d695eb4245cd34d5ff2a42
dldnjswns31 <dldnjswns32@gmail.com>
2022-12-15 05:01:22 +0900

    feat: CardList 페이지 이동을 navigate 대신 Link 컴포넌트로 수정

--- src/components/Main/Cards/CardList.jsx
+++ src/components/Main/Cards/CardList.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useActiveCategory, useCar, useSetCar } from '../../../context/CarContext';
 import { CAR_TYPE, CAR_FUEL_TYPE, CAR_CATEGORY } from '../../../utils/carAttribute';
 import getKeyByValue from '../../../utils/getKeyByValue';
@@ -7,11 +6,10 @@ import comma from '../../../utils/comma';
 import apis from '../../../apis/apis';
 import Guide from '../../common/Guide';
 import CardItem from './CardItem';
+import { Link } from 'react-router-dom';
 
 const CardList = ({ category }) => {
   const [isLoading, setIsLoading] = useState(true);
-
-  const navigate = useNavigate();
 
   const carList = useCar();
   const { activeIndex } = useActiveCategory();
@@ -28,25 +26,23 @@ const CardList = ({ category }) => {
     }
   }, [activeIndex]);
 
-  const handleClick = (id) => {
-    navigate(`/detail/${id}`);
-  };
-
   return isLoading ? (
     <Guide text="불러오는 중" />
   ) : carList.length > 0 ? (
     carList.map((car) => {
       return (
-        <li className="cardList" key={car.id} onClick={() => handleClick(car.id)}>
-          <CardItem
-            brand={car.attribute.brand}
-            name={car.attribute.name}
-            segment={getKeyByValue(CAR_TYPE, car.attribute.segment)}
-            imageUrl={car.attribute.imageUrl}
-            fuelType={getKeyByValue(CAR_FUEL_TYPE, car.attribute.fuelType)}
-            amount={comma(car.amount)}
-            createdAt={car.createdAt}
-          />
+        <li className="cardList" key={car.id}>
+          <Link to={`/detail/${car.id}`}>
+            <CardItem
+              brand={car.attribute.brand}
+              name={car.attribute.name}
+              segment={getKeyByValue(CAR_TYPE, car.attribute.segment)}
+              imageUrl={car.attribute.imageUrl}
+              fuelType={getKeyByValue(CAR_FUEL_TYPE, car.attribute.fuelType)}
+              amount={comma(car.amount)}
+              createdAt={car.createdAt}
+            />
+          </Link>
         </li>
       );
     })
